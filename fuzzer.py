fc6f3da864dfcd335cf94490b7e469c20bc90bff
Bernardi-Luca <79511920+Bernardi-Luca@users.noreply.github.com>
2023-05-19 11:12:55 +0200

    Update fuzzer.py

--- fuzzer.py
+++ fuzzer.py
@@ -1,19 +1,33 @@
 import requests
 import json
+import sys
+import time
 
-domain='aaa.com'
+infile=open(sys.argv[1],"r")
+inlines=infile.readlines()
 
-domain=domain.encode().hex()
+outfile=open("twistOut.txt", "a")
 
-x=requests.get('https://dnstwister.report/api/fuzz/'+domain)
+for line in inlines:
+        line=line.replace('\n', '')
+        line=line.replace('\r', '')
+        print("===== working on domain"+ line +" =====")
+        domain=line
+        domain=domain.encode().hex()
+        x=requests.get('https://dnstwister.report/api/fuzz/'+domain)
+        statuscode=x.status_code
+        if statuscode==200:
+                response=x.content
+                response=response.decode('utf-8')
+                response=json.loads(response)
 
-response=x.content
+                fuzz_array=response['fuzzy_domains']
 
-response=response.decode('utf-8')
+                for item in fuzz_array:
+                        print(item['domain'])
+                        outfile.write(item['domain']+"\n")
+        else:
+                print("statuscode for"+line+"is"+str(statuscode))
+        time.sleep(3)
 
-response = json.loads(response)
-
-fuzz_array=response['fuzzy_domains']
-
-for item in fuzz_array:
-        print(item['domain'])
+outfile.close()
